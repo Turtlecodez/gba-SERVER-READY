5f44ae330c6cba48ec94c284d2dda7eb9976b6a7
Logan <thecrazykid2121@gmail.com>
2023-04-27 10:03:07 -0400

    Finished

--- main/scripts/config.js
+++ main/scripts/config.js
@@ -3,15 +3,8 @@
     As of now, it's only used for the romLocation variable.
 */
 
-
-let currentPageUrl = window.location.pathname;
-let pathArray = currentPageUrl.split('/');
-let relativePath = '';
-for (let i = 1; i < pathArray.length - 2; i++) {
-  relativePath += '../';
-}
-let n64Location = relativePath + '../n64-host/';
-relativePath += '../gba-host/';
+let relativePath = 'https://rawcdn.githack.com/Math-Study/gba-host/1a4c17810e582ddf73675cd234bc979c28756a0c/';
+let n64Location = 'https://rawcdn.githack.com/Math-Study/n64-host/868f4271286e5f8813605b2a873005dc881d642a/';
 
 let ndsLocation = 'https://rawcdn.githack.com/mathadventure1/nds-host/d4fab0b759e31d196f364333ecc0ab8842cc1203/'
 
